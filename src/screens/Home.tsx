783e14d76a77b42840398623c10f361da93513c9
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-10 22:27:52 -0300

    style: styled Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,59 @@
-import { Center, Text } from 'native-base';
-
+import { ExerciseCard } from '@components/ExerciseCard';
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+import { VStack, HStack, FlatList, Heading, Text } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+  const [groups, setGroups] = useState(['costas', 'ombro', 'biceps', 'tríceps']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
+
   return (
-    <Center flex={1}>
-      <Text color='white'>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+      <FlatList
+        data={groups}
+        horizontal
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActivity={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxHeight={10}
+      />
+      <VStack flex={1} px={8}>
+
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            pb: 20,
+          }}
+        />
+      </VStack>
+
+    </VStack>
   );
 }
