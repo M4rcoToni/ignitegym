a317df675f2f11ec113ac40893a6beca080f03ff
m4rcotoni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-12 21:46:11 -0300

    style: end profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,21 +1,83 @@
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UsePhoto';
-import { Center, ScrollView, Text, VStack } from 'native-base';
-
+import { Center, ScrollView, Text, VStack, Skeleton, Heading } from 'native-base';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
       <ScrollView>
-        <Center>
-          <UserPhoto
-            source={{ uri: 'https://github.com/m4rcotoni.png' }}
-            alt='Foto do usuário'
-            size={33}
+        <Center mt={6} px={10}>
+          {
+            photoIsLoading ?
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded='full'
+                startColor='gray.500'
+                endColor='gray.400'
+              />
+              :
+              <UserPhoto
+                source={{ uri: 'https://github.com/m4rcotoni.png' }}
+                alt='Foto do usuário'
+                size={PHOTO_SIZE}
+              />
+          }
+          <TouchableOpacity>
+            <Text
+              color='green.500'
+              fontWeight='bold'
+              fontSize='md'
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            bg='gray.600'
+            placeholder='Nome'
           />
+          <Input
+            bg='gray.600'
+            placeholder='marco.pereira@gmail.com'
+            isDisabled
+          />
+
         </Center>
 
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color='gray.200' fontSize='md' mb={2}>
+            Alterar senha
+          </Heading>
+          <Input
+            bg='gray.600'
+            placeholder='Senha antiga'
+            secureTextEntry
+          />
+          <Input
+            bg='gray.600'
+            placeholder='Nova senha'
+            secureTextEntry
+          />
+          <Input
+            bg='gray.600'
+            placeholder='Confirme a nova senha'
+            secureTextEntry
+          />
+          <Button
+            mt={4}
+            title='Atualizar'
+          />
+        </VStack>
       </ScrollView>
     </VStack>
   );
