40fe95b7d306db3dadc00d5760b67708e5c02100
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-18 11:39:37 -0300

    feat: add expo-file-system

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, Text, VStack, Skeleton, Heading, useToast } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
-import { Center, ScrollView, Text, VStack, Skeleton, Heading } from 'native-base';
+import * as FileSystem from 'expo-file-system';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -12,20 +13,41 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/m4rcotoni.png');
+  const toast = useToast();
   async function handlePickPhoto() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
-    console.log(photoSelected);
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+      console.log(photoSelected);
 
-    if (photoSelected.canceled) {
-      return;
+      if (photoSelected.canceled) {
+        return;
+      }
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma até 5MB',
+            placement: 'top',
+            bgColor: 'red.500'
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
     }
-    setUserPhoto(photoSelected.assets[0].uri)
   }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
