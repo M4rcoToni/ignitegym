51b4e3718a7b4c6151727c3005eec5260de71654
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-25 11:09:39 -0300

    style: remove space

--- App.tsx
+++ App.tsx
@@ -8,7 +8,6 @@ import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
