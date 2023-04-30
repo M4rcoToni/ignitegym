d0a29b543aa997294b1646f09d40890a3a82b3ea
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-29 22:31:15 -0300

    feat: add apperror class

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,17 +1,16 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { api } from '@services/api';
 
-import axios from 'axios';
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Alert } from 'react-native';
+import { AppError } from '../utils/AppError';
 
 type FormDataProps = {
   name: string;
@@ -30,6 +29,7 @@ const signUpSchema = yup.object({
 export function SignUp() {
 
   const navigation = useNavigation();
+  const toast = useToast();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
@@ -42,13 +42,18 @@ export function SignUp() {
   async function handleSignUp({ email, name, password }: FormDataProps) {
     try {
       const response = await api.post('/users', { name, email, password })
-      console.log(response.data)
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Alert.alert(error.response?.data.message)
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível cadastrar o usuário, Tente mais tarde.';
+
+      if (isAppError) {
+        toast.show({
+          title,
+          placement: 'top',
+          bgColor: 'red.500'
+        })
       }
     }
-
   }
 
   return (
